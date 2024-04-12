d64bdf8bc565bdc4269a28b0f4fad5aa52271b9c
Kevin p <redhotkbp@hotmail.com>
2024-04-12 09:54:06 -0500

    add drop system

--- RPG/Assets/Scripts/Items and Inventory/Inventory.cs
+++ RPG/Assets/Scripts/Items and Inventory/Inventory.cs
@@ -77,7 +77,7 @@ public class Inventory : MonoBehaviour
         UpdateSlotsUI();
     }
 
-    private void UnequipItem(ItemData_Equipment itemToRemove)
+    public void UnequipItem(ItemData_Equipment itemToRemove)
     {
         if (equipmentDictionary.TryGetValue(itemToRemove, out InventoryItem value))
         {
@@ -185,4 +185,39 @@ public class Inventory : MonoBehaviour
         UpdateSlotsUI();
     }
 
+    public bool CanCraft(ItemData_Equipment _itemToCraft, List<InventoryItem> _requiredMaterials)
+    {
+        List<InventoryItem> materialsToRemove = new List<InventoryItem>();
+
+        for (int i = 0; i < _requiredMaterials.Count; i++)
+        {
+            if (stashDictionary.TryGetValue(_requiredMaterials[i].data, out InventoryItem stashValue))
+            {
+                if (stashValue.stackSize < _requiredMaterials[i].stackSize)
+                {
+                    Debug.Log("not enough materials");
+                    return false;
+                }
+                else
+                {
+                    materialsToRemove.Add(stashValue);
+                }
+            }
+            else
+            {
+                Debug.Log("not enough materials");
+                return false;
+            }
+        }
+
+        for (int i = 0; i < materialsToRemove.Count; i++)
+        {
+            RemoveItem(materialsToRemove[i].data);
+        }
+
+        AddItem(_itemToCraft);
+        Debug.Log("Here is your item " + _itemToCraft.name);
+
+        return true;
+    }
 }
