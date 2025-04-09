abc3d272819cdd0580e7dc25c1d1754859d39fbb
Kevin p <redhotkbp@hotmail.com>
2025-04-09 13:31:32 -0500

    clean up code

--- RPG/Assets/Scripts/Managers/GameManager.cs
+++ RPG/Assets/Scripts/Managers/GameManager.cs
@@ -21,8 +21,6 @@ public class GameManager : MonoBehaviour
     {
         EnemyManager.instance.ResetEnemyDeath();
         CheckpointManager.instance.ContinueGame();
-        //Scene scene = SceneManager.GetActiveScene();
-        //SceneManager.LoadScene(scene.name);
     }
 
     public void GameOver()
